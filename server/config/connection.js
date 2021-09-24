15bfdffe49245250b47a98fa87c50d39b118f93d
Lauren Norgard <lauren.c.schumann@gmail.com>
2021-09-23 21:00:22 -0500

    added auth, server, and connections.js

---
+++ server/config/connection.js
@@ -0,0 +1,14 @@
+const mongoose = require('mongoose');
+
+mongoose.connect(
+  
+  process.env.MONGODB_URI || 'mongodb://localhost/paw-mates',
+  {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true,
+    useFindAndModify: true
+  }
+);
+
+module.exports = mongoose.connection;
