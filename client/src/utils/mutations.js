f906e44a8cbd7bd612870c950383e83142dc6084
Dillon Smith <dillsmth@gmail.com>
2021-09-29 18:54:37 -0500

    renaming home

--- client/src/utils/mutations.js
+++ client/src/utils/mutations.js
@@ -1,26 +1,16 @@
 import { gql } from '@apollo/client';
 
-<<<<<<< HEAD
 export const LOGIN = gql`
   mutation login($email: String!, $password: String!) {
-=======
-export const LOGIN_USER = gql`
-  mutation loginUser($email: String!, $password: String!) {
->>>>>>> 1a1028b548c14fd1ba6bf626aaacbf43999e03d8
     login(email: $email, password: $password) {
       token
       user {
         _id
-<<<<<<< HEAD
-=======
-        username
->>>>>>> 1a1028b548c14fd1ba6bf626aaacbf43999e03d8
       }
     }
   }
 `;
 
-<<<<<<< HEAD
 export const ADD_ORDER = gql`
   mutation addOrder($products: [ID]!) {
     addOrder(products: $products) {
@@ -35,23 +25,10 @@ export const ADD_ORDER = gql`
           name
         }
       }
-=======
-export const ADD_USER = gql`
-mutation addUser($username: String!, $email: String!, $password: String!, $zipcode: String!,
-$phonenumber: String!, $puppyabout: String!, $puppyage: String, $puppybreed: String!) {
-        addUser(username: $username, email: $email, password: $password, zipcode: $zipcode,
-        phonenumber: $phonenumber, puppyabout: $puppyabout, puppyage: $puppyage, puppybreed: $puppybreed) {
-        token
-        user {
-            _id
-            username
-        }
->>>>>>> 1a1028b548c14fd1ba6bf626aaacbf43999e03d8
     }
   }
 `;
 
-<<<<<<< HEAD
 export const ADD_USER = gql`
   mutation addUser(
     $firstName: String!
@@ -72,16 +49,3 @@ export const ADD_USER = gql`
     }
   }
 `;
-=======
-// export const LIKED_PROFILE = gql`
-//   mutation likeProfile() {
-      
-//   }
-// `;
-
-// export const UNLIKE_PROFILE = gql`
-//   mutation unlikeProfile() {
-      
-//   }
-// `;
->>>>>>> 1a1028b548c14fd1ba6bf626aaacbf43999e03d8
