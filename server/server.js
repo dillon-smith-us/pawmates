f406b46ab3aa9100b5e088b395eb3044009e80f2
Dillon Smith <dillsmth@gmail.com>
2021-09-29 17:36:34 -0500

    working app, need to remove stripe, and cart

--- server/server.js
+++ server/server.js
@@ -1,35 +1,27 @@
-// Dependencies
-// Express Server
 const express = require('express');
-// Node path
-const path = require('path');
-// Import the Apollo Server
 const { ApolloServer } = require('apollo-server-express');
-// Import the typeDefs and resolvers
+const path = require('path');
+
 const { typeDefs, resolvers } = require('./schemas');
 const { authMiddleware } = require('./utils/auth');
-// Database connection
 const db = require('./config/connection');
 
-// Set up the Express server
-const app = express();
 const PORT = process.env.PORT || 3001;
-
-// Set up the Apollo Server and pass in the schema
+const app = express();
 const server = new ApolloServer({
   typeDefs,
   resolvers,
   context: authMiddleware
 });
 
-// integrate the Apollo server with the Express application as middleware
 server.applyMiddleware({ app });
 
-// Express middleware for parsing
-app.use(express.urlencoded({ extended: true }));
+app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
-// if we're in production, serve client/build as static assets
+// Serve up static assets
+app.use('/images', express.static(path.join(__dirname, '../client/images')));
+
 if (process.env.NODE_ENV === 'production') {
   app.use(express.static(path.join(__dirname, '../client/build')));
 }
@@ -38,7 +30,6 @@ app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname, '../client/build/index.html'));
 });
 
-// GraphQL and Express Server start
 db.once('open', () => {
   app.listen(PORT, () => {
     console.log(`API server running on port ${PORT}!`);
